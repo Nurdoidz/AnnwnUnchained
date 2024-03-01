59ed81f31326070b0e2281ec1d34d978399f6739
Nurdoidz <nurdoidz@live.com>
2024-03-01 02:09:15 -0700

    Initial mod list

--- build.ps1
+++ build.ps1
@@ -21,6 +21,17 @@ if ($Clean) {
 
 Write-Host "$(TimeCode) Executing task: build"
 
+$Modlist = [System.IO.File]::ReadLines("$(Resolve-Path .\pack\)modlist.html") | Sort-Object
+$Modlist[0] = '<ul>'
+$Modlist[$Modlist.Length - 1] = '</ul>'
+$Modlist > .\pack\modlist.html
+Write-Host "$(TimeCode) Sorted mod list."
+
+$Readme = Get-Content .\src\README-template.md -Raw
+$Readme = $Readme.Replace('<!-- MODLIST -->', ($Modlist))
+$Readme > .\README.md
+Write-Host "$(TimeCode) Generated README from template."
+
 [System.IO.Directory]::CreateDirectory('.\build\') | Out-Null
 Write-Debug "$(TimeCode) Created directory: build"
 [System.IO.Directory]::CreateDirectory('.\build\artifact\') | Out-Null
@@ -31,13 +42,20 @@ if ($null -eq (Get-Command '7z' -ErrorAction SilentlyContinue)) {
     Write-Host "$(TimeCode) Stopping due to errors."
     return
 }
+if ($null -eq (Get-Command 'jq' -ErrorAction SilentlyContinue)) {
+    Write-Error "$(TimeCode) `"jq`" was not found in PATH."
+    Write-Host "$(TimeCode) Stopping due to errors."
+    return
+}
 
-$Manifest = Get-Content .\src\manifest.json -Raw | ConvertFrom-Json
+$Manifest = (jq '.files|=sort_by(.projectID)' .\pack\manifest.json) -join("`n")
+$Manifest > .\pack\manifest.json
+$Manifest = Get-Content .\pack\manifest.json -Raw | ConvertFrom-Json
 Write-Host "$(TimeCode) Found artifact name: $($Manifest.name)"
 Write-Host "$(TimeCode) Found artifact version: $($Manifest.version)"
 Remove-Item -Force .\build\artifact\*.zip
 $ArtifactName = "$($Manifest.name)-v$($Manifest.version)".Replace(' ', '-')
-7z u "$(Resolve-Path .\build\artifact\)$ArtifactName.zip" "$(Resolve-Path .\src\)*" | Out-Null
+7z u "$(Resolve-Path .\build\artifact\)$ArtifactName.zip" "$(Resolve-Path .\pack\)*" | Out-Null
 Write-Host "$(TimeCode) Created artifact: $(Resolve-Path .\build\artifact\)$ArtifactName.zip"
 
 Write-Host "$(TimeCode) Completed task: build"
---
+++ src/README-template.md
@@ -0,0 +1,29 @@
+<h1 align="center"><img width=240 alt="Logo" src="https://raw.githubusercontent.com/Nurdoidz/AnnwnUnchained/master/img/square-logo.png"/><br>Annwn Unchained</h1>
+
+Annwn Unchained is a carefully-crafted Minecraft modpack for [CurseForge](https://www.curseforge.com/) in the theme of steampunk and medieval, in a pre-industrial era.
+
+## Mod List
+
+<!-- MODLIST -->
+
+## Building
+
+The following are requirements for building the project:
+
+- [PowerShell](https://github.com/PowerShell/PowerShell) v7+, available as `pwsh` in `PATH` or as the shell for running the scripts in this project.
+- [7-zip](https://sourceforge.net/projects/sevenzip/files/7-Zip/) v23+, available as `7z` in `PATH`.
+- [jq](https://github.com/jqlang/jq) v1.7+, available as `jq` in `PATH`.
+
+To build the project, first clone the project to a local directory. Then, navigate to the cloned project directory in a shell and run the following command:
+
+```shell
+pwsh -ExecutionPolicy Bypass -NoProfile -File ./build.ps1
+```
+
+Alternatively, launch PowerShell, navigate to the cloned project directory, and run the following command:
+
+```powershell
+. .\build.ps1 -ExecutionPolicy Bypass -NoProfile
+```
+
+The generated artifacts will be available in `build/artifacts/` and can be imported directly into the CurseForge launcher.
